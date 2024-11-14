c8358f1e282d48c77f77f7fe6c13ea3628978ed4
Noe Rabinovici <norabino@c1r3p9.42perpignan.fr>
2024-11-14 11:01:32 +0100

    auto update

--- ft_print_x.c
+++ ft_print_x.c
@@ -1,33 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_print_x.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/14 08:45:41 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/14 10:22:14 by norabino         ###   ########.fr       */
+/*   Created: 2024/11/14 10:17:36 by norabino          #+#    #+#             */
+/*   Updated: 2024/11/14 10:17:50 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdarg.h>
-
-#include <stdio.h> //temporaire
-
-int	ft_printf(const char *str, ...);
-
-int	ft_print_c(void);
-int	ft_print_s(void);
-int	ft_print_p(void);
-int	ft_print_d(void);
-int	ft_print_i(void);
-int	ft_print_u(void);
-int	ft_print_x(void);
-int	ft_print_X(void);
-
-#endif
+int	ft_print_x(void)
+{
+	printf("hexa lower");
+	return 1;
+}
--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,11 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 11:42:04 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/14 09:52:23 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/14 10:56:05 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 int	ft_isconv(char c)
 {
@@ -29,31 +28,68 @@ int	ft_isconv(char c)
 	return (0);
 }
 
-int	ft_printf(const char *str, ...)
+int	ft_printchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+void	ft_conversions(va_list ap, const char *format, int	*i, int *len)
+{
+	if (format[*i + 1] && format[*i + 1] == 'c')
+		*len += ft_print_c();
+	if (format[*i + 1] && format[*i + 1] == 's')
+		*len +=ft_print_s();
+	if (format[*i + 1] && format[*i + 1] == 'p')
+		*len +=ft_print_p();
+	if (format[*i + 1] && format[*i + 1] == 'i')
+		*len +=ft_print_i();
+	if (format[*i + 1] && format[*i + 1] == 'u')
+		*len +=ft_print_u();
+	if (format[*i + 1] && format[*i + 1] == 'd')
+		*len +=ft_print_d();
+	if (format[*i + 1] && format[*i + 1] == 'x')
+		*len +=ft_print_x();
+	if (format[*i + 1] && format[*i + 1] == 'X')
+		*len +=ft_print_x();
+	return ;
+}
+
+int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
 	int		len;
 	int		i;
+	char newline = '\n';
 	
-	va_start(ap, str);
+	va_start(ap, format);
 	len = 0;
 	i = 0;
-	while (str[i])
+	while (format[i] != '%' && format[i])
 	{
-		while (str[i] != '%')
-		{
-			write(1, &str[i], 1);
-			i++;
-		}
+		write(1, &format[i], 1);
 		i++;
-		//if (str[i] == '%' && ft_isconv(str[i + 1]))
-			//...
+		len++;
+	}
+	while (format[i])
+	{
+		if (format[i] == '%' && ft_isconv(format[i + 1]))
+		{	
+			if (format[i + 1] == '%')
+				len += ft_printchar('%');	
+			else
+				ft_conversions(ap, format, &i, &len);
+			i += 2;
+			continue;
+		}
+		len += ft_printchar(format[i]);
+		i++;
 	}
 	return (len);
 }
 
 int	main(void)
 {
-	ft_printf("allo");
+	ft_printf("allo%d bonjour");
 	return 0;
 }
