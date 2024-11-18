eb19e0970bd2e4eb2ffdb8eadaa0856eb98957fa
Noe Rabinovici <norabino@c2r6p8.42perpignan.fr>
2024-11-18 17:02:35 +0100

    auto update

--- ft_print_p.c
+++ ft_print_p.c
@@ -6,20 +6,22 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 10:14:15 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/18 15:47:18 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/18 16:59:39 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_print_p(unsigned long nb)
+int	ft_print_p(unsigned long long nb)
 {
 	int		len;
 
 	len = 0;
 	if (nb == 0)
 		return (ft_print_s("(nil)"));
-	if (nb > 0)
+	//if (nb >= 18446744073709551615 - 1)
+		//len += ft_print_s("18446744073709551615");
+	else if (nb > 0)
 	{
 		len += ft_print_s("0x");
 		ft_putnbr_base(nb, "0123456789abcdef", &len);
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 11:42:04 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/18 16:04:21 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/18 17:02:21 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,15 +77,15 @@ int	ft_printf(const char *format, ...)
 	return (len);
 }
 
-/*int	main(void)
+int	main(void)
 {
 	int	i;
 	int	j;
 
 	printf("\noutput of real:\n");
-	i = printf(" %p %p ", 429467295, -429467295);
+	i = printf("%p", -2147483647 - 1);
 	printf("\n\noutput of fake:\n");
-	j = ft_printf(" %p %p ", 429467295, -429467295);
+	j = ft_printf("%p", -2147483647 - 1);
 	printf("\n\nreal:%d,fake:%d",i,j);
 	return 0;
-}*/
+}
