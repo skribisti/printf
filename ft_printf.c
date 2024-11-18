cb72f167c85544a163ec8a2254be970e339ef8f0
Noe Rabinovici <norabino@c2r6p8.42perpignan.fr>
2024-11-18 15:38:25 +0100

    auto update

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 11:42:04 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/18 15:28:29 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/18 15:38:24 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,15 +77,15 @@ int	ft_printf(const char *format, ...)
 	return (len);
 }
 
-/*int	main(void)
+int	main(void)
 {
 	int	i;
 	int	j;
 
 	printf("\noutput of real:\n");
-	i = printf("%X", -999999999912);
+	i = printf("%p", -1);
 	printf("\n\noutput of fake:\n");
-	j = ft_printf("%X", -999999999912);
+	j = ft_printf("%p", -1);
 	printf("\n\nreal:%d,fake:%d",i,j);
 	return 0;
-}*/
+}
