5f36ed35fd9e53608b965e2e6c4281ec8307eacb
Noe Rabinovici <norabino@c2r6p8.42perpignan.fr>
2024-11-18 14:58:00 +0100

    auto update

--- ft_print_p.c
+++ ft_print_p.c
@@ -6,27 +6,11 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 10:14:15 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/18 14:56:11 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/18 14:57:53 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	ft_put_ptr(long num)
-{
-	if (num >= 16)
-	{
-		ft_put_ptr(num / 16);
-		ft_put_ptr(num % 16);
-	}
-	else
-	{
-		if (num <= 9)
-			ft_print_c(num + '0');
-		else
-			ft_print_c(num - 10 + 'a');
-	}
-}
 
 int	ft_print_p(unsigned long long nb)
 {
@@ -40,11 +24,6 @@ int	ft_print_p(unsigned long long nb)
 	len += ft_print_s("0x");
 	if (n > 0)
 		ft_putnbr_base(n, "0123456789abcdef", &len);
-	if (n < 0)
-	{
-		ft_put_ptr(n);
-	}
+	len += ft_size(n, 16);
 	return (len);
 }
-
-//len += ft_size((int *)n, 16);	ligne 46
